30483a25554ebc6aca1989ea71d58b00a3b7b813
luck2901 <luck2901@gmail.com>
2021-02-17 10:35:33 +0900

    2/17

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,21 +2,25 @@ import React from "react";
 import {HashRouter as Router, Route, Switch} from "react-router-dom";
 import Auth from "../routes/Auth";
 import Home from "../routes/Home";
+import Register from "../routes/Register";
 
 const AppRouter = ({isLoggedIn}) =>{
     return (
         <Router>
             <Switch>
-                {isLoggedIn ? (
+                {isLoggedIn ? ( //Log In : true
                     <>
                     <Route exact path="/">
                         <Home/>
                     </Route>
                     </>
-                ) : (
-                    <>
+                ) : ( //Log In : false
+                    <> 
                     <Route exact path="/">
                         <Auth/>
+                    </Route>
+                    <Route exact path="/Register">
+                        <Register />
                     </Route>
                     </>
                 )}
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,9 +1,11 @@
 import React, {useState} from "react";
+import {useHistory} from "react-router-dom"
 import "firebase/auth";
 
 const Auth = () => {
     const [email,setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const history = useHistory();
 
     const onChange = (e) =>{
         const {target:{value, name}} = e;
@@ -13,11 +15,15 @@ const Auth = () => {
     const onSubmit = (e) =>{
         e.preventDefault();
     }
+    const onClick = () =>{
+        history.push("/Register");
+    }
     return(
         <form onSubmit = {onSubmit}>
             <input name = "email" type="text" onChange = {onChange} value={email} placeholder="Email"/>
             <input name = "password" type ="password" onChange={onChange} value={password} placeholder="Password"/>
             <input type="submit" value="LOG IN"/>
+            <input type="submit" onClick={onClick} value="Register"/>
         </form>
     );
 }
