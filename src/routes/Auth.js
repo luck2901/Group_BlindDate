1ecfb9b386358b7a3db5ffdee446080448ab81e0
luck2901 <luck2901@gmail.com>
2021-02-17 10:15:51 +0900

    2/17

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,5 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
+import "firebase/auth";
 
-const Auth = () =><span>auth</span>
+const Auth = () => {
+    const [email,setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const onChange = (e) =>{
+        const {target:{value, name}} = e;
+        if(name === "email") setEmail(value);
+        else if(name ==="password") setPassword(value);
+    }
+    const onSubmit = (e) =>{
+        e.preventDefault();
+    }
+    return(
+        <form onSubmit = {onSubmit}>
+            <input name = "email" type="text" onChange = {onChange} value={email} placeholder="Email"/>
+            <input name = "password" type ="password" onChange={onChange} value={password} placeholder="Password"/>
+            <input type="submit" value="LOG IN"/>
+        </form>
+    );
+}
 
 export default Auth;
