5b5eeacdc4302700ec20cf39975eafb1dc6a4300
luck2901 <luck2901@gmail.com>
2021-02-18 10:14:44 +0900

    2/18

--- src/routes/Register.js
+++ src/routes/Register.js
@@ -4,20 +4,37 @@ const Register = () =>{
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [school, setSchool] = useState("");
-    onChange=(e) =>{
+    const [name, setName] = useState("");
+    const [age, setAge] = useState("");
+    const [gender, setGender] = useState("man");
+
+    const onChange=(e) =>{
         const {target:{value,name}}=e;
-        if(name===email) setEmail(value);
-        else if(name===password) setPassword(value);
-        else if(name===school) setSchool(value);
+        if(name==="email") setEmail(value);
+        else if(name==="password") setPassword(value);
+        else if(name==="school") setSchool(value);
+        else if(name==="age") setAge(value);
+        else if(name==="name") setName(value);
     }
-    onSubmit = (e) =>{
+    const onSubmit = (e) =>{
         e.preventDefault();
+    }
+    const onCheck = (e) =>{
+        const {target:{value, checked}} = e;
+        if(checked){
+            if(value==="man") setGender(value);
+            else if(value==="woman") setGender(value);    
+        }
     }
     return (
         <form onSubmit={onSubmit}>
             <input type="email" placeholder="Email" name="email" onChange = {onChange} required/>
             <input type="password" placeholder="Password" name="password" onChange = {onChange} required/>
             <input type="text" placeholder= "School" name="school" onChange = {onChange} required/>
+            <input type="text" placeholder="Name" name="name" onChange={onChange} required />
+            <input type="text" placeholder="Age" name="age" onChange={onChange} required />
+            <input type="radio" name="gender" value="man"  onClick={onCheck} defaultChecked/>Man
+            <input type="radio" name="gender" value="woman" onClick={onCheck} />Woman
         </form>
     );
 }
