8b2555bf45fee6e09b540acd87e084ad9687fc0a
luck2901 <luck2901@gmail.com>
2021-02-19 21:52:17 +0900

    2/19

--- src/routes/Register.js
+++ src/routes/Register.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react"
+import {useHistory} from "react-router-dom"
+import {authService} from "../Fbase"
 
 const Register = () =>{
     const [email, setEmail] = useState("");
@@ -7,6 +9,8 @@ const Register = () =>{
     const [name, setName] = useState("");
     const [age, setAge] = useState("");
     const [gender, setGender] = useState("man");
+    const[error,setError] = useState("");
+    const history = useHistory();
 
     const onChange=(e) =>{
         const {target:{value,name}}=e;
@@ -16,8 +20,14 @@ const Register = () =>{
         else if(name==="age") setAge(value);
         else if(name==="name") setName(value);
     }
-    const onSubmit = (e) =>{
+    const onSubmit = async(e) =>{
         e.preventDefault();
+        try{
+            const data = await authService.createUserWithEmailAndPassword(email,password);
+        }catch(error){
+            setError(error.message)
+        };
+
     }
     const onCheck = (e) =>{
         const {target:{value, checked}} = e;
@@ -26,15 +36,21 @@ const Register = () =>{
             else if(value==="woman") setGender(value);    
         }
     }
+    const onBack =() =>{
+        history.push("/");
+    } 
     return (
         <form onSubmit={onSubmit}>
-            <input type="email" placeholder="Email" name="email" onChange = {onChange} required/>
-            <input type="password" placeholder="Password" name="password" onChange = {onChange} required/>
-            <input type="text" placeholder= "School" name="school" onChange = {onChange} required/>
-            <input type="text" placeholder="Name" name="name" onChange={onChange} required />
-            <input type="text" placeholder="Age" name="age" onChange={onChange} required />
+            <input type="email" placeholder="Email" name="email" onChange = {onChange} required/> <br/>
+            <input type="password" placeholder="Password" name="password" onChange = {onChange} required/> <br/>
+            <input type="text" placeholder= "School" name="school" onChange = {onChange} /> <br/>
+            <input type="text" placeholder="Name" name="name" onChange={onChange}  /> <br/>
+            <input type="text" placeholder="Age" name="age" onChange={onChange}  /> <br/>
             <input type="radio" name="gender" value="man"  onClick={onCheck} defaultChecked/>Man
-            <input type="radio" name="gender" value="woman" onClick={onCheck} />Woman
+            <input type="radio" name="gender" value="woman" onClick={onCheck} />Woman <br/>
+            <input type="submit" name="register" value = "Submit"/>
+            <button name="back" value = "back" onClick={onBack}>Back</button> <br/>
+            <span>{error}</span>
         </form>
     );
 }
