c78051a01224992e183a53a3352542ee6aa37aa6
luck2901 <luck2901@gmail.com>
2021-02-23 22:30:35 +0900

    2/23

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,7 +1,7 @@
 import React, {useState} from "react";
 import {useHistory} from "react-router-dom"
 import "firebase/auth";
-import { authService } from '../Fbase';
+import { authService, firebaseInstance } from '../Fbase';
 
 const Auth = () => {
     const [email,setEmail] = useState("");
@@ -22,16 +22,27 @@ const Auth = () => {
             setError(error.message);
         }
     }
-    const onClick = () =>{
-        history.push("/Register");
+    const onClick = async(e) =>{
+        const {target:{name}} = e;
+        let provider;
+        if(name === "register")
+        {
+            history.push("/Register");
+        }else if(name==="google"){
+            provider = new firebaseInstance.auth.GoogleAuthProvider();
+        }        
+        await authService.signInWithPopup(provider);
     }
     return(
         <form onSubmit = {onSubmit}>
             <input name = "email" type="text" onChange = {onChange} value={email} placeholder="Email" required/>
             <input name = "password" type ="password" onChange={onChange} value={password} placeholder="Password" required/>
             <input type="submit" value="LOG IN"/>
-            <input type="submit" onClick={onClick} value="Register"/>
+            <input name = "register" type="submit" onClick={onClick} value="Register"/>
             <span>{error}</span>
+            <div>
+                <button onClick={onClick} name="google">LOGIN with GOOGLE</button>
+            </div>
         </form>
     );
 }
