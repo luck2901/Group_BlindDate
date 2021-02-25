713a9de405aad84ce4d47d7d723ed51fb72ac71e
unknown <luck2901@naver.com>
2021-02-25 11:27:27 +0900

    2/25

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React,{useState} from "react";
 
 const Home = () =>{
     const [name, setName] = useState("");
     const [age, setAge] = useState("");
-    const [sex, setSex] = useState("");
+    const [sex, setSex] = useState("남");
+
+    const onRadioClick = (e) =>{
+        const {target:{value}} = e;
+        if(value==="남") setSex("남");
+        else if(value==="여") setSex("여");
+    }
     return(
-    
+        <div>
+            <form>
+                <input name="name" value={name} type ="text" placeholder="이름이 뭐에요?" /> <br/>
+                <input name="age" value={age} type="text" placeholder="나이는요??" /><br/>
+                <label><input name="sex" value="남" type="radio" onClick={onRadioClick} defaultChecked/>남</label>
+                <label><input name="sex" value="여" type="radio" onClick={onRadioClick}/>여</label> <br/>
+                <span>{sex}</span>
+            </form>
+        </div>
     );
 }
 
