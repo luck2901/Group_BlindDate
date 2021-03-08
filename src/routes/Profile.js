41e54217128d59ae4dad4f5ea7d9a290cd1fbd0e
DESKTOP-1J72GLA\HanSangHyeon <luck2901@naver.com>
2021-03-08 17:59:50 +0900

    3/8

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -33,8 +33,8 @@ const Profile = ({userObject}) =>{
                 <input name="age" value={age} type="text" onChange = {onChange} required placeholder="나이는요??" /><br/>
                 <label><input name="sex" value="남" type="radio" onClick={onRadioClick} defaultChecked/>남</label>
                 <label><input name="sex" value="여" type="radio" onClick={onRadioClick}/>여</label> <br/>
-                <input type="submit" value="등록" />
             </form>
+            <input type="submit" value="Edit"/>
             <button onClick={onClick}>LOG OUT</button>
         </div>
     );
