b37e563edcdd0063cefea0960bc2dda94b73e261
DESKTOP-1J72GLA\HanSangHyeon <luck2901@naver.com>
2021-03-09 19:53:43 +0900

    3/9

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,24 @@
-import React,{} from "react";
+import React, { useEffect } from 'react';
 
-const Home = () =><span>ss</span>
+const { kakao } = window;
 
-export default Home;
+const Home = () => {
+
+    useEffect(() => {
+        const container = document.getElementById('myMap');
+		const options = {
+			center: new kakao.maps.LatLng(33.450701, 126.570667),
+			level: 3
+		};
+        const map = new kakao.maps.Map(container, options);
+    }, []);
+
+    return (
+        <div id='myMap' style={{
+            width: '500px', 
+            height: '500px'
+        }}></div>
+    );
+}
+
+export default Home; 
