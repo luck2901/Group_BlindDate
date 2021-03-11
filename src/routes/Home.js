9a2af05bf0d8e6cb61a6e2f94eda071245d4b23f
한상현 <luck2901@naver.com>
2021-03-11 17:16:15 +0900

    3/11

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -5,20 +5,37 @@ const { kakao } = window;
 const Home = () => {
 
     useEffect(() => {
-        const container = document.getElementById('myMap');
-		const options = {
-			center: new kakao.maps.LatLng(33.450701, 126.570667),
-			level: 3
-		};
+        const container = document.getElementById('myMap'); //지도를 담을 영역의 DOM 레퍼런스.
+        const options = {
+            center: new kakao.maps.LatLng(35.844126, 127.131557), //지도의 중심좌표.
+            level: 5 //지도의 레벨(확대, 축소 정도)
+        };
         const map = new kakao.maps.Map(container, options);
+        const clusterer = new kakao.maps.MarkerClusterer({
+            map: map,
+            averageCenter: true,
+            minlevel: 2,
+            styles: [{
+                width: '53px', height: '52px',
+                background: 'url(cluster.png) no-repeat',
+                color: '#fff',
+                textAlign: 'center',
+                lineHeight: '54px'
+            }]
+        });
+        const marker = new kakao.maps.Marker({
+            position: new kakao.maps.LatLng(35.843628, 127.127320)
+        })
+        clusterer.addMarker(marker);
+
     }, []);
 
     return (
         <div id='myMap' style={{
-            width: '500px', 
+            width: '500px',
             height: '500px'
         }}></div>
     );
 }
 
-export default Home; 
+export default Home;
