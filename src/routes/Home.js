01ebe2fb99fc4cde40f24a9eb43e702fedd8df57
한상현 <luck2901@naver.com>
2021-03-13 15:51:14 +0900

    3/13

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -3,7 +3,8 @@ import React, { useEffect, useState } from 'react';
 const { kakao } = window;
 
 const Home = () => {
-    const [information, setInformation] = useState('');
+    const [title, setTitle] = useState('');
+    const [information, setInformation] = useState("");
 
     useEffect(() => {
         const container = document.getElementById('myMap'); //지도를 담을 영역의 DOM 레퍼런스.
@@ -24,16 +25,61 @@ const Home = () => {
                 lineHeight: '54px'
             }]
         });
+        if (navigator.geolocation) {
+
+            // GeoLocation을 이용해서 접속 위치를 얻어옵니다
+            navigator.geolocation.getCurrentPosition(function (position) {
+
+                let lat = position.coords.latitude, // 위도
+                    lon = position.coords.longitude; // 경도
+
+                let locPosition = new kakao.maps.LatLng(lat, lon) // 마커가 표시될 위치를 geolocation으로 얻어온 좌표로 생성합니다
+
+                // 마커와 인포윈도우를 표시합니다
+                displayMarker(locPosition);
+
+            });
+
+        } else { // HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
+
+            let locPosition = new kakao.maps.LatLng(33.450701, 126.570667)
+
+            displayMarker(locPosition);
+        }
+
+
+        // 지도에 마커와 인포윈도우를 표시하는 함수입니다
+        function displayMarker(locPosition) {
+
+            // 마커를 생성합니다
+            let marker = new kakao.maps.Marker({
+                map: map,
+                position: locPosition
+            });
+
+            // 지도 중심좌표를 접속위치로 변경합니다
+            map.setCenter(locPosition);
+        }
         const position = [
             {
                 title: '베스킨 라빈스 31',
                 latlng: new kakao.maps.LatLng(35.843628, 127.127320),
-                content: '베스킨 라빈스 31'
+                content: '맛있는 걸 먹었으면 시~원한 아이스크림 어떠세요??'
             },
             {
                 title: '서래 갈매기',
                 latlng: new kakao.maps.LatLng(35.842662, 127.128264),
                 content: '서래 갈매기'
+            },
+            {
+                title: '오늘김해뒷고기',
+                latlng: new kakao.maps.LatLng(35.843023, 127.128489),
+                content: '김뒷'
+            },
+            {
+                title: '홍곱창',
+                latlng: new kakao.maps.LatLng(35.842040, 127.128102),
+                content: '홍곱창'
             }
         ]
         const imageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
@@ -47,6 +93,7 @@ const Home = () => {
                 image: markerImage
             });
             kakao.maps.event.addListener(marker, 'click', function () {
+                setTitle(position[i].title);
                 setInformation(position[i].content);
             });
         }
@@ -59,8 +106,13 @@ const Home = () => {
                 height: '500px'
             }}>
             </div>
-            <div id="information">
-                {information}
+            <div >
+                <div>
+                    {title}
+                </div>
+                <div>
+                    {information}
+                </div>
             </div>
         </div>
     );
