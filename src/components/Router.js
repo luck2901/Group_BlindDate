5792805cba8ebd737b54c7cb144d0cb2bf28715b
한상현 <luck2901@naver.com>
2021-03-15 14:47:15 +0900

    3/15

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,34 +1,38 @@
 import React from "react";
-import {HashRouter as Router, Redirect, Route, Switch} from "react-router-dom";
+import { HashRouter as Router, Redirect, Route, Switch } from "react-router-dom";
 import Auth from "../routes/Auth";
 import Home from "../routes/Home";
 import Profile from "../routes/Profile";
 import Register from "../routes/Register";
 import Navigation from "./Navigation";
+import Chat from "../routes/Chat";
 
-const AppRouter = ({isLoggedIn, userObject}) =>{
+const AppRouter = ({ isLoggedIn, userObject }) => {
     return (
         <Router>
-            {isLoggedIn && <Navigation/>}
+            {isLoggedIn && <Navigation />}
             <Switch>
                 {isLoggedIn ? ( //Log In : true
                     <>
-                    <Route exact path="/">
-                        <Home userObject={userObject}/>
-                    </Route>
-                    <Route exact path="/profile">
-                        <Profile userObject={userObject}/>
-                    </Route>
+                        <Route exact path="/">
+                            <Home userObject={userObject} />
+                        </Route>
+                        <Route exact path="/profile">
+                            <Profile userObject={userObject} />
+                        </Route>
+                        <Route exact path="/Chat">
+                            <Chat userObject={userObject} />
+                        </Route>
                     </>
                 ) : ( //Log In : false
-                    <> 
-                    <Route exact path="/">
-                        <Auth/>
-                    </Route>
-                    <Route exact path="/Register">
-                        <Register userObject={userObject}/>
-                    </Route>
-                    <Redirect from="*" to="/"/>
+                    <>
+                        <Route exact path="/">
+                            <Auth />
+                        </Route>
+                        <Route exact path="/Register">
+                            <Register userObject={userObject} />
+                        </Route>
+                        <Redirect from="*" to="/" />
                     </>
                 )}
             </Switch>
---
+++ src/routes/Chat.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Chat = () => <span>chatting</span>
+
+export default Chat;
