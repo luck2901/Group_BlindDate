270d1c4da7199b53802947e87a9da1e7901cb512
한상현 <luck2901@naver.com>
2021-03-16 15:41:47 +0900

    3/16

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -5,7 +5,7 @@ const { kakao } = window;
 const Home = () => {
     const [title, setTitle] = useState('');
     const [information, setInformation] = useState("");
-    const [kind, setKind] = useState("cafe");
+    const [kind, setKind] = useState("food");
 
     useEffect(() => {
         const container = document.getElementById('myMap'); //지도를 담을 영역의 DOM 레퍼런스.
@@ -78,6 +78,46 @@ const Home = () => {
                     content: '맛있는 걸 먹었으면 시~원한 아이스크림 어떠세요??'
                 },
             ]
+        } else if (kind === "tour") {
+            position = [
+                {
+                    title: '한국소리문화의전당',
+                    latlng: new kakao.maps.LatLng(35.855508, 127.138294),
+                    content: '전국에서 여섯 번째로 문화행사와 공연이 많이 열리는 곳이며, 도시로 치면 서울과 부산 다음가는 수치를 보여주는 한국 소리문화의 전당입니다.'
+                },
+                {
+                    title: '덕진체련공원',
+                    latlng: new kakao.maps.LatLng(35.853134, 127.140868),
+                    content: '덕진체련공원은 스포츠 시설로 축구 경기장, 테니스 경기장, 배드민턴 경기장이 갖춰져 있습니다. '
+                },
+                {
+                    title: '전주동물원',
+                    latlng: new kakao.maps.LatLng(35.857816, 127.144227),
+                    content: '좋은 시설과 동물들의 자유, 생태 환경을 제공하는 전주동물원으로 오세요. 벚꽃이 피는 시기에는 야간개장도 열린답니다!'
+                },
+
+                {
+                    title: '전북대학교 건지광장',
+                    latlng: new kakao.maps.LatLng(35.846902, 127.129397),
+                    content: '전통 누각과 지당, 병풍 조형물, 청운정, 원형수반 등 가장 한국적인 캠퍼스 전북대를 보러 오세요~'
+                },
+                {
+                    title: '덕진공원',
+                    latlng: new kakao.maps.LatLng(35.848963, 127.122888),
+                    content: '전주 시민들의 낭만이 고스란히 담긴 덕진공원으로 당신을 초대합니다.'
+                },
+
+                {
+                    title: '전북대학교 중앙도서관',
+                    latlng: new kakao.maps.LatLng(35.848319, 127.131920),
+                    content: '전북대학교를 둘러보다 쉬고싶을 때 중앙도서관으로 와보세요'
+                },
+                {
+                    title: '전북대학교 신정문',
+                    latlng: new kakao.maps.LatLng(35.841110, 127.131825),
+                    content: '가장 한국적인 캠퍼스 전북대학교로 들어가는 한옥 정문입니다. 밤에 더 이뻐요'
+                },
+            ]
         }
 
         const imageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
@@ -103,6 +143,8 @@ const Home = () => {
             setKind("food");
         } else if (name === "카페") {
             setKind("cafe");
+        } else if (name === "관광지") {
+            setKind("tour");
         }
 
     }
@@ -117,6 +159,7 @@ const Home = () => {
                 <div>
                     <button onClick={onSelect} name="음식점">음식점</button>
                     <button onClick={onSelect} name="카페">카페</button>
+                    <button onClick={onSelect} name="관광지">관광지</button>
                 </div>
                 <div>
                     {title}
