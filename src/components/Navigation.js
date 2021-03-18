d7d4e02d5951d1fbe49cd4eefd5eb19455e3d4cf
한상현 <luck2901@naver.com>
2021-03-18 19:39:32 +0900

    3/18

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,7 +5,7 @@ const Navigation = () => {
     return (
         <nav>
             <div className="nav-wrapper">
-                <a href="#" class="brand-logo">Welcome JBNU</a>
+                <a href="#" className="brand-logo">Welcome JBNU</a>
                 <ul id="nav-mobile" className="right hide-on-mid-and-down">
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="/Chat">Chat</Link></li>
