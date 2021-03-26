255e14d7f19419c1bacc4f9d3f895173a403d9ba
한상현 <luck2901@naver.com>
2021-03-26 16:28:45 +0900

    3/26

--- src/Fbase.js
+++ src/Fbase.js
@@ -1,6 +1,7 @@
 import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore"
+import 'firebase/database';
 
 const firebaseConfig = {
     apiKey: "AIzaSyAu-JZUATnhcEdLn4MldrXMkfKHlXC_iZE",
@@ -9,12 +10,14 @@ const firebaseConfig = {
     storageBucket: "ugbd-f2a5b.appspot.com",
     messagingSenderId: "552713815669",
     appId: "1:552713815669:web:d117ff3dc1052eccc8da3c"
-  };
+};
 
-  firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
 
-  export const authService = firebase.auth();
+export const authService = firebase.auth();
 
-  export const dbService = firebase.firestore();
+export const dbService = firebase.firestore();
 
-  export const firebaseInstance = firebase;
+export const firebaseInstance = firebase;
+
+export const chatDb = firebase.database();
--- src/routes/Chat.js
+++ src/routes/Chat.js
@@ -1,5 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import {chatDb} from "../Fbase";
 
-const Chat = () => <span>chatting</span>
+const Chat = () => {
+    const [item, setItem] = useState("");
+    useEffect(() => {
+        const tmp = chatDb.ref('user/chat').on('value', (snapshot) => {
+            const data = snapshot.val();
+            setItem(data);
+        })
+    })
+    return (
+        <span>{item}</span>
+    );
+}
 
 export default Chat;
