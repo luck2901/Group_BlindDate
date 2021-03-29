f44221894c3279a2faee4aac0ad14e0ec53d08cb
한상현 <luck2901@naver.com>
2021-03-29 11:21:20 +0900

    3/29

--- src/components/Chatting.js
+++ src/components/Chatting.js
@@ -1,13 +1,53 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useState } from "react"
 import { dbService } from "../Fbase";
+import {faTrash, faPencilAlt} from "@fortawesome/free-solid-svg-icons"
 
-const Chatting = ({ chatObject, isOwner }) => {
+const Chatting = ({ chatObj, isOwner }) => {
     const [editing, setEditing] = useState(false);
-    const [newChat, setNewChat] = useState(chatObject.chat);
+    const [newChat, setNewChat] = useState(chatObj.chat);
     const onDeleteClick = async () => {
         const ok = window.confirm("Are you sure you want to delete this Chat?");
         if (ok) {
-            await dbService.doc(`chats.${chatObject.id}`).delete();
+            await dbService.doc(`chats/${chatObj.id}`).delete();
         }
+    };
+    const toggleEditing = () => setEditing((prev) => !prev);
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        await dbService.doc(`chats/${chatObj.id}`).update({
+            chat: newChat,
+        });
+        setEditing(false);
     }
+    const onChange = (e) => {
+        const { targetL: { value } } = e;
+        setNewChat(value);
+    }
+    return (
+        <div>
+            {
+                editing ? (
+                    <>
+                        <form onSubmit={onSubmit} >
+                            <input onChange={onChange} type="text" placeholder="Edit" value={newChat} required autoFocus />
+                            <input type = "submit" value ="Update Chat" />
+                        </form>
+                        <button onClick={toggleEditing} > Cancel</button>
+                    </>
+                ) :
+                    <>
+                        <h4>{chatObj.chat}</h4>
+                        {isOwner && (
+                            <div>
+                                <span onClick={onDeleteClick}><FontAwesomeIcon icon={faTrash} /></span>
+                                <span onClick={toggleEditing}><FontAwesomeIcon icon={faPencilAlt} /></span>
+                            </div>
+                        )}
+                    </>
+            }
+        </div>
+    )
 }
+
+export default Chatting;
